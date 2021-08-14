52c6d3bea1668e2ab8bc6d7b3f242ca8d3ef0042
Sheikhasib <sheikhhasib037@gmail.com>
2021-08-14 12:32:43 +0600

    okk

--- js/events.js
+++ js/events.js
@@ -1,3 +1,14 @@
 function makeRed(){
     document.body.style.backgroundColor = 'red';
 }
+
+// handle blue button click by setting function name
+
+const blueButton = document.getElementById('make-blue-button');
+console.log(blueButton);
+// just set the name of the function
+blueButton.onclick = makeBlue();
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
